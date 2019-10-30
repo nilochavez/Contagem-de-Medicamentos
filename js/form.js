da4d47a3b762fe448ab2f68eaa7a003efad7c4dc
nilochavez <54970125+nilochavez@users.noreply.github.com>
2019-10-30 17:25:19 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -27,8 +27,50 @@ var carboplatinaTd50 = document.querySelector(".frascos-carbo50");
 var totalCisplatinaTd = document.querySelector(".total-cisplatina");
 var cisplatinaTd = document.querySelector(".frascos-cisplatina");
 
+var totalCitarabinaTd = document.querySelector(".total-citarabina");
+var citarabinaTd = document.querySelector(".frascos-citarabina");
 
+var totalDocetaxel80Td = document.querySelector(".total-docetaxel80");
+var docetaxel80Td = document.querySelector(".frascos-docetaxel80");
 
+var totalDocetaxel20Td = document.querySelector(".total-docetaxel20");
+var docetaxel20Td = document.querySelector(".frascos-docetaxel20");
+
+var totalEunadesTd = document.querySelector(".total-eunades");
+var eunadesTd = document.querySelector(".frascos-eunades");
+
+var totalFluoracilaTd = document.querySelector(".total-fluoracila");
+var fluoracilaTd = document.querySelector(".frascos-fluoracila");
+
+var totalGencitabina1000Td = document.querySelector(".total-gencitabina1000");
+var gencitabina1000Td = document.querySelector(".frascos-gencitabina1000");
+
+var totalGencitabina200Td = document.querySelector(".total-gencitabina200");
+var gencitabina200Td = document.querySelector(".frascos-gencitabina200");
+
+var totalHoloxaneTd2 = document.querySelector(".total-holoxane2");
+var holoxaneTd2 = document.querySelector(".frascos-holoxane2");
+
+var totalHoloxaneTd1 = document.querySelector(".total-holoxane1");
+var holoxaneTd1 = document.querySelector(".frascos-holoxane1");
+
+var totalHoloxaneTd500 = document.querySelector(".total-holoxane500");
+var holoxaneTd500 = document.querySelector(".frascos-holoxane500");
+
+var totalFludarabinaTd = document.querySelector(".total-fludarabina");
+var fludarabinaTd = document.querySelector(".frascos-fludarabina");
+
+var totalIrinotecanoTd100 = document.querySelector(".total-irinotecano100");
+var irinotecanoTd100 = document.querySelector(".frascos-irinotecano100");
+
+var totalIrinotecanoTd40 = document.querySelector(".total-irinotecano40");
+var irinotecanoTd40 = document.querySelector(".frascos-irinotecano40");
+
+var totalEloxatinTd = document.querySelector(".total-eloxatin");
+var eloxatinTd = document.querySelector(".frascos-eloxatin");
+
+var totalPaclitaxelTd = document.querySelector(".total-paclitaxel");
+var paclitaxelTd = document.querySelector(".frascos-paclitaxel");
 
 function validaCampo(){
 
@@ -51,6 +93,11 @@ else {
 }
 
 };
+
+function LimpaCampo() {
+  document.querySelector("#form-principal").reset();
+};
+
 
 // insere a dose no array do medicamento selelecionado
 function validaCisplatina(){
@@ -97,9 +144,78 @@ if(opcoes.value == "1"){
 };
 };
 
-function LimpaCampo() {
-  document.querySelector("#form-principal").reset();
-};
+function validaCitarabina(){
+
+if(opcoes.value == "6"){
+	citarabina.push(campo.value);
+    console.log(citarabina);
+}};
+
+function validaDocetaxel(){
+
+if(opcoes.value == "7"){
+	docetaxel.push(campo.value);
+    console.log(docetaxel);
+}};
+
+function validaEundes(){
+
+if(opcoes.value == "8"){
+	eunades.push(campo.value);
+    console.log(eunades);
+}};
+
+
+function validaFluoracila(){
+
+if(opcoes.value == "9"){
+	fluoracila.push(campo.value);
+    console.log(fluoracila);
+}};
+
+function validaGencitabina(){
+
+if(opcoes.value == "10"){
+	gencitabina.push(campo.value);
+    console.log(gencitabina);
+}};
+
+function validaHoloxane(){
+
+if(opcoes.value == "11"){
+	holoxane.push(campo.value);
+    console.log(holoxane);
+}};
+
+function validaFludarabina(){
+
+if(opcoes.value == "12"){
+	fludarabina.push(campo.value);
+    console.log(fludarabina)
+}};
+
+function validaIrinotecano(){
+
+if(opcoes.value == "13"){
+	irinotecano.push(campo.value);
+    console.log(irinotecano)
+}};
+
+function validaEloxatin(){
+
+if(opcoes.value == "14"){
+	eloxatin.push(campo.value);
+    console.log(eloxatin);
+}};
+
+function validaPaclitaxel(){
+
+if(opcoes.value == "15"){
+	paclitaxel.push(campo.value);
+    console.log(paclitaxel);
+}};
+
+
 
 
 
@@ -120,18 +236,38 @@ validaGenuxal();
 validaVelcade();
 validaCarboplatina();
 validaCisplatina();
+validaCitarabina();
+validaDocetaxel();
+validaEundes();
+validaFluoracila();
+validaGencitabina();
+validaHoloxane();
+validaFludarabina();
+validaIrinotecano();
+validaEloxatin();
+validaPaclitaxel();
 
 somaFrascosVelcade();
 somaFrascosGenuxal();
 somaFrascosVidaza();
 somaFrascosCarboplatina();
 somaFrascosCisplatina();
-
+somaFrascosCitarabina();
+somaFrascosDocetaxel();
+somaFrascosEunades();
+somaFrascosFluoracila();
+somaFrascosGencitabina();
+somaFrascosHoloxane();
+somaFrascosFludarabina();
+somaFrascosIrinotecano();
+somaFrascosEloxatin();
+somaFrascosPaclitaxel();
 
 
 var totalVelcade = somaFrascosVelcade();
 var totalGenuxal = somaFrascosGenuxal();
 var totalVidaza = somaFrascosVidaza();
+
 
 
 // arredonda a quantidade de frascos pra cima
@@ -140,6 +276,12 @@ velcadeTd.textContent = Math.ceil(totalFrascosVelcade);
 vidazaTd.textContent = Math.ceil(totalFrascosVidaza);
 cisplatinaTd.textContent = Math.ceil(totalFrascosCisplatina);
 genuxalTd.textContent = Math.ceil(totalFrascosGenuxal);
+citarabinaTd.textContent = Math.ceil(totalFrascosCitarabina);
+eunadesTd.textContent = Math.ceil(totalFrascosEunades);
+fluoracilaTd.textContent = Math.ceil(totalFrascosFluoracila);
+fludarabinaTd.textContent = Math.ceil(totalFrascosFludarabina);
+eloxatinTd.textContent = Math.ceil(totalFrascosEloxatin);
+paclitaxelTd.textContent = Math.ceil(totalFrascosPaclitaxel);
 
 LimpaCampo();
 
--- js/principal.js
+++ js/principal.js
@@ -3,12 +3,34 @@ var totalFrascosVelcade = 0;
 var totalFrascosGenuxal = 0;
 var totalFrascosVidaza = 0;
 var totalFrascosCisplatina = 0;
+var totalFrascosCitarabina = 0;
+var totalFrascosEunades = 0;
+var totalFrascosFluoracila = 0;
+var totalFrascosFludarabina = 0;
+var totalFrascosEloxatin = 0;
+var totalFrascosPaclitaxel = 0;
 
 var totalCarboplatina = 0;
 var addTabela50 = 0;
 var addTabela450 = 0;
 var addTabela150 = 0;
 
+var totalDocetaxel = 0;
+var addTabelaDoce80 = 0;
+var addTabelaDoce20 = 0;
+
+var totalGencitabina = 0;
+var addTabelaGenci1000 = 0;
+var addTabelaGenci200 = 0;
+
+var totalHoloxane = 0;
+var addTabelaHoloxane2 = 0;
+var addTabelaHoloxane1 = 0;
+var addTabelaHoloxane500 = 0;
+
+var totalIrinotecano = 0;
+var addTabelaIrintecano100 = 0;
+var addTabelaIrintecano40 = 0;
 
 
 
@@ -17,7 +39,16 @@ var genuxal = [];
 var velcade = [];
 var carboplatina = [];
 var cisplatina = [];
-
+var citarabina = [];
+var docetaxel = [];
+var eunades = [];
+var fluoracila = [];
+var gencitabina = [];
+var holoxane = [];
+var fludarabina = [];
+var irinotecano = [];
+var eloxatin = [];
+var paclitaxel = [];
 
 
 function somaFrascosVelcade(){
@@ -112,8 +143,217 @@ function somaFrascosCisplatina(){
  	totalCisplatinaTd.textContent = totalCisplatina;
  	 
 }};
- 	 
 
+function somaFrascosCitarabina(){
+	
+	var totalCitarabina = 0;
+	
+	for(var i = 0; i < citarabina.length; i++){
+ 	
+ 	totalCitarabina = parseFloat(totalCitarabina) + parseFloat(citarabina[i]);
+ 	totalFrascosCitarabina = (totalCitarabina / 500);
+ 	totalCitarabinaTd.textContent = totalCitarabina;
+ 	 
+}};
+
+
+function somaFrascosDocetaxel(){
+	
+	for(var i = 0; i < docetaxel.length; i++){
+ 	
+ 	totalDocetaxel = parseFloat(totalDocetaxel) + parseFloat(docetaxel[i]);
+ 	totalDocetaxel80Td.textContent = totalDocetaxel; 
+ 	console.log(totalDocetaxel);
+
+ 	
+ if  (parseFloat(docetaxel[i]) > 60){
+
+ 		while(docetaxel[i] > 60) {
+ 		docetaxel[i] = (docetaxel[i] - 80)
+ 	 	
+ 	 	addTabelaDoce80 = addTabelaDoce80 + 1;
+ 	 	docetaxel80Td.textContent = addTabelaDoce80;
+ 	 	console.log(totalDocetaxel);
+ 	 	
+ 	 	
+}};
+
+if  (parseFloat(docetaxel[i]) > 20 && docetaxel[i] <= 60){
+
+ 		while(docetaxel[i] > 0 ) {
+ 		docetaxel[i] = (docetaxel[i] - 20)
+ 	 	
+ 	 	addTabelaDoce20 = addTabelaDoce20 + 1;
+ 	 	docetaxel20Td.textContent = addTabelaDoce20;
+ 	 	console.log(docetaxel[i]);
+ 	 	
+}}}};
+
+function somaFrascosEunades(){
+	
+	var totalEunades = 0;
+	
+	for(var i = 0; i < eunades.length; i++){
+ 	
+ 	totalEunades = parseFloat(totalEunades) + parseFloat(eunades[i]);
+ 	totalFrascosEunades = (totalEunades / 100);
+ 	totalEunadesTd.textContent = totalEunades;
+
+ }};
+
+ function somaFrascosFluoracila(){
+	
+	var totalFluoracila = 0;
+	
+	for(var i = 0; i < fluoracila.length; i++){
+ 	
+ 	totalFluoracila = parseFloat(totalFluoracila) + parseFloat(fluoracila[i]);
+ 	totalFrascosFluoracila = (totalFluoracila / 2500);
+ 	totalFluoracilaTd.textContent = totalFluoracila;
+
+ }};
+
+function somaFrascosGencitabina(){
+	
+	for(var i = 0; i < gencitabina.length; i++){
+ 	
+ 	totalGencitabina = parseFloat(totalGencitabina) + parseFloat(gencitabina[i]);
+ 	totalGencitabina1000Td.textContent = totalGencitabina; 
+ 	console.log(totalGencitabina);
+
+ 	
+ if  (parseFloat(gencitabina[i]) >= 900){
+
+ 		while(gencitabina[i] >= 900) {
+ 		gencitabina[i] = (gencitabina[i] - 1000)
+ 	 	
+ 	 	addTabelaGenci1000 = addTabelaGenci1000 + 1;
+ 	 	gencitabina1000Td.textContent = addTabelaGenci1000;
+ 	 	console.log(totalGencitabina);
+ 	 	
+ 	 	
+}};
+
+if  (parseFloat(gencitabina[i]) > 0 && gencitabina[i] <= 800){
+
+ 		while(gencitabina[i] > 0 ) {
+ 		gencitabina[i] = (gencitabina[i] - 200)
+ 	 	
+ 	 	addTabelaGenci200 = addTabelaGenci200 + 1;
+ 	 	gencitabina200Td.textContent = addTabelaGenci200;
+ 	 	console.log(gencitabina[i]);
+ 	 	
+}}}};
+
+function somaFrascosHoloxane(){
+	
+	for(var i = 0; i < holoxane.length; i++){
+ 	
+ 	totalHoloxane = parseFloat(totalHoloxane) + parseFloat(holoxane[i]);
+ 	totalHoloxaneTd2.textContent = totalHoloxane; 
+ 	console.log(totalHoloxane);
+ 	
+ if  (parseFloat(holoxane[i]) > 1500){
+
+ 		while(holoxane[i] > 1500) {
+ 		holoxane[i] = (holoxane[i] - 2000)
+ 	 	
+ 	 	addTabelaHoloxane2 = addTabelaHoloxane2 + 1;
+ 	 	holoxaneTd2.textContent = addTabelaHoloxane2;
+ 	 	console.log(totalHoloxane);
+ 	 	
+ 	 	
+}};
+
+if  (parseFloat(holoxane[i]) > 500 && holoxane[i] <= 1000){
+
+ 		while(holoxane[i] > 500) {
+ 		holoxane[i] = (holoxane[i] - 1000)
+ 	 	
+ 	 	addTabelaHoloxane1 = addTabelaHoloxane1 + 1;
+ 	 	holoxaneTd1.textContent = addTabelaHoloxane1;
+ 	 	console.log(holoxane[i]);
+ 	 	
+}};
+
+if  (parseFloat(holoxane[i]) > 0 && holoxane[i] <= 500){
+	
+ 		while(holoxane[i] > 0) {
+ 		holoxane[i] = (holoxane[i] - 500)
+ 	 	
+ 	 	addTabelaHoloxane500 = addTabelaHoloxane500 + 1;
+ 	 	holoxaneTd500.textContent = addTabelaHoloxane500;
+ 	 	console.log(holoxane[i]);
+ 	 	
+ 	 	
+}}}};
+
+function somaFrascosFludarabina(){
+	
+	var totalFludarabina = 0;
+	
+	for(var i = 0; i < fludarabina.length; i++){
+ 	
+ 	totalFludarabina = parseFloat(totalFludarabina) + parseFloat(fludarabina[i]);
+ 	totalFrascosFludarabina = (totalFludarabina / 50);
+ 	totalFludarabinaTd.textContent = totalFludarabina;
+
+ }};
+
+ function somaFrascosIrinotecano(){
+	
+	for(var i = 0; i < irinotecano.length; i++){
+ 	
+ 	totalIrinotecano = parseFloat(totalIrinotecano) + parseFloat(irinotecano[i]);
+ 	totalIrinotecanoTd100.textContent = totalIrinotecano; 
+ 	console.log(totalIrinotecano);
+
+ 	
+ if  (parseFloat(irinotecano[i]) >= 80){
+
+ 		while(irinotecano[i] >= 80) {
+ 		irinotecano[i] = (irinotecano[i] - 100)
+ 	 	
+ 	 	addTabelaIrintecano100 = addTabelaIrintecano100 + 1;
+ 	 	irinotecanoTd100.textContent = addTabelaIrintecano100;
+ 	 	console.log(totalIrinotecano);
+}};
+
+if  (parseFloat(irinotecano[i]) > 0 && irinotecano[i] <= 80){
+
+ 		while(irinotecano[i] > 0 ) {
+ 		irinotecano[i] = (irinotecano[i] - 40)
+ 	 	
+ 	 	addTabelaIrintecano40 = addTabelaIrintecano40 + 1;
+ 	 	irinotecanoTd40.textContent = addTabelaIrintecano40;
+ 	 	console.log(irinotecano[i]);
+}}}};
+
+function somaFrascosEloxatin(){
+	var totalEloxatin = 0;
+	for(var i = 0; i < eloxatin.length; i++){
+ 	totalEloxatin = parseFloat(totalEloxatin) + parseFloat(eloxatin[i]);
+ 	 
+ 	 totalFrascosEloxatin = (totalEloxatin / 100);
+	 
+	 totalEloxatinTd.textContent = totalEloxatin;
+	 eloxatinTd.textContent = totalFrascosEloxatin;
+	
+ 	 
+}};
+
+function somaFrascosPaclitaxel(){
+	var totalPaclitaxel = 0;
+	for(var i = 0; i < paclitaxel.length; i++){
+ 	totalPaclitaxel = parseFloat(totalPaclitaxel) + parseFloat(paclitaxel[i]);
+ 	 
+ 	 totalFrascosPaclitaxel = (totalPaclitaxel / 100);
+	 
+	 totalPaclitaxelTd.textContent = totalPaclitaxel;
+	 paclitaxelTd.textContent = totalFrascosPaclitaxel;
+	
+ 	 
+}};
 
 //---------------------------------------------------- tabela soros
 var totalequiposimples = 0;
